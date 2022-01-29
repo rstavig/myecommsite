1c0989f48de2f1af970a948b81a8817ade25b756
Bob Stavig <rstavig@Bob-Stavigs-iMac.local>
2022-01-29 11:11:56 -0600

    try again to publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -27,21 +27,25 @@ connectDB();
 //   res.send(data.users);
 // });
 
-app.get("/api/config/paypal", (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID);
-});
-
 app.use("/api/uploads", uploadRouter);
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID);
+});
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.get("/", (req, res) => {
-  res.send("Server is running on port 5000");
-});
+// app.get("/", (req, res) => {
+//   res.send("Server is running on port 5000");
+// });
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
