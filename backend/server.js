f5edb1d6f8e16dbf8e8407ca81e5de2d172fb857
Bob Stavig <rstavig@Bob-Stavigs-iMac.local>
2022-01-31 23:02:02 -0600

    server change

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: true }));
 connectDB();
 
 // mongoose.connect("mongodb://localhost:27017/bobazon", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
+// useNewUrlParser: true,
+// useUnifiedTopology: true,
 // });
 
 // app.get("/api/users", (req, res) => {
@@ -36,12 +36,10 @@ app.get("/api/config/paypal", (req, res) => {
 });
 
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
-// app.use(express.static(path.join(__dirname, "/frontend/build")));
-// app.get("*", (req, res) =>
-//   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-// );
+app.use(express.static(path.join(__dirname, "frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "frontend/build/index.html"))
+);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
