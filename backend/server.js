8fc32102f1723072c89a29beefb9d0211c9cb8ce
Bob Stavig <rstavig@Bob-Stavigs-iMac.local>
2022-02-01 16:09:18 -0600

    rmvd PORT in env file

--- backend/server.js
+++ backend/server.js
@@ -36,19 +36,14 @@ app.get("/api/config/paypal", (req, res) => {
 });
 
 const __dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
   app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
-});
-} else {
+  app.get("*", (req, res) => 
+    res.sendFile(path.join(__dirname, "/frontend", "/build", "/index.html"))
+);
   app.get("/", (req, res) => {
-    res.send("Api Running")
+    res.send("Server is Running")
   });
-}
-
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
