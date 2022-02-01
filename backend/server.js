c7a0d270b5af44ee742c7417ae1f01188f55ed2f
Bob Stavig <rstavig@Bob-Stavigs-iMac.local>
2022-02-01 13:24:33 -0600

    moved again

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 // import colors from "colors";
-import connectDB from "./backend/config/db.js";
+import connectDB from "./config/db.js";
 // import mongoose from "mongoose";
-import productRouter from "./backend/routers/productRouter.js";
-import userRouter from "./backend/routers/userRouter.js";
-import orderRouter from "./backend/routers/orderRouter.js";
-import uploadRouter from "./backend/routers/uploadRouter.js";
+import productRouter from "./routers/productRouter.js";
+import userRouter from "./routers/userRouter.js";
+import orderRouter from "./routers/orderRouter.js";
+import uploadRouter from "./routers/uploadRouter.js";
 
 dotenv.config();
 
