2eb6a7ea57450a08d87db9ab04f88a7bedb240cc
Bob Stavig <rstavig@Bob-Stavigs-iMac.local>
2022-02-02 15:15:20 -0600

    another try

--- backend/server.js
+++ backend/server.js
@@ -36,11 +36,13 @@ app.get("/api/config/paypal", (req, res) => {
 });
 
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.use(express.static(path.join(__dirname,"/frontend/build")));
   app.get("*", (req, res) => 
-    res.sendFile(path.join(__dirname, "/frontend", "/build", "/index.html"))
+    res.sendFile(path.join(__dirname,"/frontend", "/build","/index.html"))
 );
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
   app.get("/", (req, res) => {
     res.send("Server is Running")
   });
