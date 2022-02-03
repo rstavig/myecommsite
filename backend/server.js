f24a6ed23c82ec0e4e68d65a944cfe7545f39968
Bob Stavig <rstavig@Bob-Stavigs-iMac.local>
2022-02-03 13:54:36 -0600

    changed server

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ const __dirname = path.resolve();
 
   app.use(express.static(path.join(__dirname,"/frontend/build")));
   app.get("*", (req, res) => 
-    res.sendFile(path.join(__dirname,"/frontend", "/build","/index.html"))
+    res.sendFile(path.join(__dirname,"/frontend/build/index.html"))
 );
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
