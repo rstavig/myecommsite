d3727aee1fd3c2812450258f2aba117fb7a16dc0
Bob Stavig <rstavig@Bob-Stavigs-iMac.local>
2022-02-11 19:55:33 -0600

    for push to gh

--- backend/server.js
+++ backend/server.js
@@ -38,24 +38,22 @@ app.get("/api/config/paypal", (req, res) => {
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   app.get('*', (req, res) => 
     res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
-} else {
-  app.get("/", (req, res) => {
-    res.send("Server is Running")
-  });
 
-}
+  // app.get("/", (req, res) => {
+  //   res.send("Server is Running")
+  // });
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
